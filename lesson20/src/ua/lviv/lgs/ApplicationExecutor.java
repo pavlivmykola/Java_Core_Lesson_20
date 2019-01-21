eb90b6496ecf908d36b70449d00585d6f1b2c824
Pavliv Mykola <mpavliv@ukr.net>
2019-01-22 00:00:23 +0200

    Threads

--- lesson20/src/ua/lviv/lgs/ApplicationExecutor.java
+++ lesson20/src/ua/lviv/lgs/ApplicationExecutor.java
@@ -8,53 +8,65 @@ import java.util.concurrent.Executors;
 
 public class ApplicationExecutor {
 
+	public static synchronized void input(boolean direction) {
+		ArrayList<Integer> arrayFibonachi = new ArrayList<Integer>();
+		Scanner sc = new Scanner(System.in);
+		if (direction) 
+		System.out.println("Введіть ціле число кількість чисел Фібоначі");
+		 else System.out.println("Введіть ціле число кількість чисел Фібоначі зворотній порядок");
+		int number = sc.nextInt();
+		for (int i = 0; i < number; i++) {
+			switch (i) {
+			case 0:
+				arrayFibonachi.add(1);
+				break;
+			case 1:
+				arrayFibonachi.add(1);
+				break;
+			default:
+				arrayFibonachi.add(arrayFibonachi.get(i - 2) + arrayFibonachi.get(i - 1));
+			}
+		}
+		
+		try {
+			Thread.currentThread().sleep(1000);
+		} catch (InterruptedException e) {
+
+		}
+		Iterator<Integer> iterator = arrayFibonachi.iterator();
+		if (direction) {
+			while (iterator.hasNext()) {
+				System.out.println(iterator.next());
+			}
+		} else {
+			for (int i = arrayFibonachi.size() - 1; i >= 0; i--) {
+				System.out.println(arrayFibonachi.get(i));
+			}
+		}
+
+	}
+
 	public static void main(String[] args) {
+
 		ExecutorService executor = Executors.newFixedThreadPool(2);
 
 		executor.execute(new Runnable() {
 
 			@Override
 			public void run() {
-				ArrayList<Integer> arrayFibonachi = new ArrayList<Integer>();
-				Scanner sc = new Scanner(System.in);
-				System.out.println("Введіть ціле число кількість чисел Фібоначі");
-				int number = sc.nextInt();
-				for (int i = 0; i < number; i++) {
-					switch (i) {
-					case 0:
-						arrayFibonachi.add(1);
-						break;
-					case 1:
-						arrayFibonachi.add(1);
-						break;
-					default:
-						arrayFibonachi.add(arrayFibonachi.get(i - 2) + arrayFibonachi.get(i - 1));
-					}
-				}
+				input(true);
+			}
+		});
+		executor.execute(new Runnable() {
 
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					
-				}
-				Iterator<Integer> iterator = arrayFibonachi.iterator();
-				while (iterator.hasNext()) {
-					System.out.println(iterator.next());
-				}
-				System.out.println("Зворотній порядок");
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					
-				}
-				for (int i=arrayFibonachi.size()-1;i>=0;i--) {
-					System.out.println(arrayFibonachi.get(i));
-				}
-
+			@Override
+			public void run() {
+				input(false);
 			}
 
 		});
 
+		executor.shutdown();
 	}
 
 }
